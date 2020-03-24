2d4000223912e475ef8624a3b9d289f6cac28dda
Teerasak61070094 <42912705+Teerasak61070094@users.noreply.github.com>
2020-03-24 17:11:47 +0700

    refactor code

---
+++ js/js.js
@@ -0,0 +1,15 @@
+function price1() {
+    var sold = "64,900 Baht";
+    var pr1 = sold.fontcolor("#7E8FFF").fontsize(6);
+    document.getElementById("show").innerHTML = pr1;
+}
+function price2() {
+    var sold = "75,900 Baht";
+    var pr1 = sold.fontcolor("#7E8FFF").fontsize(6);
+    document.getElementById("show").innerHTML = pr1;
+}
+function price3() {
+    var sold = "89,900 Baht";
+    var pr1 = sold.fontcolor("#7E8FFF").fontsize(6);
+    document.getElementById("show").innerHTML = pr1;
+}
